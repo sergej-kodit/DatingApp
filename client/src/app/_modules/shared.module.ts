04ac8246fe4f933182f6c1e4aedb80555fa23b8e
Sergej Manojlovic <sergej.manojlovic@kodit.rs>
2022-10-19 11:16:09 +0200

    End of section 11

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FileUploadModule } from 'ng2-file-upload';
 
 @NgModule({
   declarations: [],
@@ -11,7 +12,8 @@ import { TabsModule } from 'ngx-bootstrap/tabs';
       positionClass: 'toast-bottom-right',
     }),
     TabsModule.forRoot(),
+    FileUploadModule,
   ],
-  exports: [ToastrModule, TabsModule],
+  exports: [ToastrModule, TabsModule, FileUploadModule],
 })
 export class SharedModule {}
